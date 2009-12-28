abcdd76d2625043e667902bba8501bc4060721ca
Robert Bohne <rbo@openserv.org>
2009-12-28 19:50:14 +0100

    Add groupby support for stats

--- lib/Nagios/MKLivestatus/Class/Abstract/Stats.pm
+++ lib/Nagios/MKLivestatus/Class/Abstract/Stats.pm
@@ -1,13 +1,51 @@
-package # Hide from pause
+package # Hide from pause 
     Nagios::MKLivestatus::Class::Abstract::Stats;
 
 use Moose;
 use Carp;
 extends 'Nagios::MKLivestatus::Class::Base::Abstract';
 
+my $TRACE = $Nagios::MKLivestatus::Class::TRACE || 0;
+
 sub build_mode { return 'Stats'; };
 
 sub build_compining_prefix { return 'Stats'; }
+
+sub build_operators {
+    my $self = shift;
+    my $operators = $self->SUPER::build_operators();
+
+    push @{ $operators }, {
+        regexp   => qr/(groupby)/ix,
+        handler => '_cond_op_groupby',
+    };
+
+    return $operators;
+}
+
+sub _cond_op_groupby {
+    my $self    = shift;
+    my $operator = shift;
+    my $value = shift;
+    my $combining_count = shift || 0;
+
+    print STDERR "#IN  _cond_op_groupby $operator $value $combining_count\n" if $TRACE > 9;
+
+    my ( @child_statment ) = $self->_dispatch_refkind($value, {
+        SCALAR  => sub {
+            return ( sprintf("%s%s: %s",$self->compining_prefix,$operator,$value) );
+        },
+    });
+
+    print STDERR "#OUT _cond_op_groupby $operator $value $combining_count\n" if $TRACE > 9;
+    return ( $combining_count, @child_statment );
+}
+
+
+# {
+#     regex   => qr/(groupby)/ix,
+#     handler => '_cond_compining',
+# }
 
 1;
 __END__
--- t/30-stats.t
+++ t/30-stats.t
@@ -6,12 +6,22 @@ use_ok('Nagios::MKLivestatus::Class::Abstract::Stats');
 
 my @testings = (
     { name => 'localhost' }, ["Stats: name = localhost"],
+    [
+        { state => [0,1,2,3] },
+        { '-groupby' => 'host_name'}
+    ], [
+        "Stats: state = 0",
+        "Stats: state = 1",
+        "Stats: state = 2",
+        "Stats: state = 3",
+        "StatsGroupBy: host_name",
+    ],
 );
 
 for ( my $i = 0 ; $i < scalar @testings ; $i++ ) {
     my $search            = $testings[$i];
     my $expected_statment = $testings[ ++$i ];
-    my $filter_obj         = Nagios::MKLivestatus::Class::Abstract::Stats->new();
+    my $filter_obj        = Nagios::MKLivestatus::Class::Abstract::Stats->new();
     my $got_statment;
     eval {
         $got_statment = $filter_obj->apply($search);
