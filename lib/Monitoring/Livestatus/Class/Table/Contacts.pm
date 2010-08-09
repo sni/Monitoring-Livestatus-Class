4c97ab26be2dc4db5739096dcc65f081895ccb92
Robert Bohne <rbo@cpan.org>
2010-08-09 11:12:31 +0200

    some POD improvements

--- lib/Monitoring/Livestatus/Class/Table/Contacts.pm
+++ lib/Monitoring/Livestatus/Class/Table/Contacts.pm
@@ -4,6 +4,11 @@ package # hide from pause
 use Moose;
 extends 'Monitoring::Livestatus::Class::Base::Table';
 
+sub build_table_name { return 'contacts' };
+
+1;
+__END__
+
 =head1 NAME
 
 Monitoring::Livestatus::Class::Table::Contacts - Class for contacts table
@@ -14,13 +19,9 @@ Monitoring::Livestatus::Class::Table::Contacts - Class for contacts table
 
 Returns the table name from these class.
 
-=cut
-
-sub build_table_name { return 'contacts' };
-
 =head1 AUTHOR
 
-Robert Bohne, C<< <rbo at cpan.org> >>
+See L<Monitoring::Livestatus::Class/AUTHOR> and L<Monitoring::Livestatus::Class/CONTRIBUTORS>.
 
 =head1 COPYRIGHT & LICENSE
 
@@ -33,4 +34,3 @@ by the Free Software Foundation; or the Artistic License.
 See http://dev.perl.org/licenses/ for more information.
 
 =cut
-1;
